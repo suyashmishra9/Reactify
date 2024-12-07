a7c0ce19ad98d475844c0281522f6dbc74820c86
Suyash Mishra <suyashmishra@blubirch.com>
2024-12-08 00:07:24 +0530

    initial commit

---
+++ basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Component from "./Comp";
+
+
+function App() {
+  return (
+    <>
+      <Component/>
+    <h1>Hello React Js </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ basicreact/src/Comp.js
@@ -0,0 +1,7 @@
+function Component() {
+  return (
+    <h2>This is the component </h2>
+  )
+}
+
+export default Component
