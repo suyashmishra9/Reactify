01eb6314bc59e98614f15aab4e519aac49fdb385
Suyash Mishra <suyashmishra1008@gmail.com>
2025-01-04 19:06:17 +0530

    changes

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind CSS </h1>
       <Card  channel = "Chaiaurcode"/>
       <Card  channel= "Suyash React"/>
-
     </>
   )
 }
 
 export default App
+ff
