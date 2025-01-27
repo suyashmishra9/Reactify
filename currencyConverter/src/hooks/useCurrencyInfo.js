3a8c1c9badce033ac4479ae1d498768db20f7a79
Suyash Mishra <suyashmishra1008@gmail.com>
2025-01-27 15:57:53 +0530

    changes in console and view of curency and url hits

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({})
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`).then((res) => res.json()).then((res) => setData(res[currency]))
-    }, [currency])
-    console.log(data)
-    return [data] ; 
-} 
+    const [data, setData] = useState({});
 
-export default useCurrencyInfo 
+    useEffect(() => {
+        const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+        console.log(`Fetching data from URL: ${url}`);
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(`Response received for currency: ${currency}`, res[currency]);
+                setData(res[currency]);
+            })
+    }, [currency]);
+
+    return [data];
+}
+
+export default useCurrencyInfo;
