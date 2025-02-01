182fd3af7ecba2df8b275c28a6268f927a3e46fd
Suyash Mishra <suyashmishra1008@gmail.com>
2025-02-01 17:24:32 +0530

    profile checked

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -1,12 +1,14 @@
-import React, {useContext} from 'react'
-import UserContext from '../context/UserContext'
+import React, { useContext } from 'react';
+import UserContext from '../context/UserContext';
 
 function Profile() {
-    const {user} = useContext(UserContext)
+    const { user } = useContext(UserContext);
     
-    if (!user) return <div>please login</div>
+    if (!user || !user.username) {
+        return <div>Please login</div>;
+    }
 
-    return <div>Welcome {user.username}</div>
+    return <div>Welcome {user.username}</div>;
 }
 
-export default Profile
+export default Profile;
