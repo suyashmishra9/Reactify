bd97fbdf93716357ae4c28fbfc0840f9294a6f67
Suyash Mishra <suyashmishra1008@gmail.com>
2025-02-17 20:12:06 +0530

    redux toolkit setup

---
+++ redux-toolkit-todo/src/features/todo/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    todos: [{ id: 1, text: "Hello World" }]
+}
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+              const todo  = {
+                id:  nanoid() , 
+                text: action.payload.text
+              }
+              state.todos.push(todo)
+        },
+        removeTodo: (state ,action) => {
+            state.todos = state.todos.filter((todo) => todo.id != action.payload)
+        },
+    }
+})
