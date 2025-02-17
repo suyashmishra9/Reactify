fde8661751289b30d0c60ad3449f3b17a9e984a5
Suyash Mishra <suyashmishra1008@gmail.com>
2025-02-17 13:34:52 +0530

    some debugging

--- todoContextLocal/src/components/TodoItem.jsx
+++ todoContextLocal/src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import { useTodo } from '../contexts/ToDoContext';
 
 function TodoItem({ todo }) {
     
-    const {updateTodo , deleteTodo, toggleComplete}  = useTodo
+    const {updateTodo , deleteTodo, toggleComplete}  = useTodo()
     const [isTodoEditable , setIsTodoEditable] = useState(false)
     const [todoMsg, setTodoMsg] = useState(todo.todo)
 
