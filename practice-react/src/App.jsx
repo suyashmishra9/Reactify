2218d28bd5d7c38cfeb0a124c3705152940a1994
Suyash Mishra <suyashmishra1008@gmail.com>
2025-02-18 12:37:06 +0530

    practice jsx

--- practice-react/src/App.jsx
+++ practice-react/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import { useState , useMemo, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const countFunc = () =>{
+    setCount(count +1 )
+  }
+
+  const heavyFunc = (count) =>{
+    for (let i = 0; i < 1000; i++) {
+    }
+    console.log("Executing heavy function");
+    return count ; 
+  }
+
+  const heavy = useCallback(() => heavyFunc(count) , [count])
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Uses of Usememo() and useCallback()</h1>
+     <h4>The counter value is : {heavy()}</h4>
+     <button onClick={countFunc}>Click </button>
     </>
   )
 }
